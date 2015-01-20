cf08b9a4de24c5408e7e01504fe7e4ee2ffc209f
Ponpoorani Ravichandran <pr1224@nyu.edu>
2015-01-20 00:59:28 -0500

    Modifies icon,added code to display numbers
    Error with /

--- AdvancedCalc/app/src/main/java/com/nyu/calc/advancedcalc/MainActivity.java
+++ AdvancedCalc/app/src/main/java/com/nyu/calc/advancedcalc/MainActivity.java
@@ -4,15 +4,155 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    Button one, two, three, four, five, six, seven, eight, nine, zero, add, sub, mul, div, equal,clr;
+    EditText res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+/*
+        one = (Button) findViewById(R.id.one);
+        two = (Button) findViewById(R.id.two);
+        three = (Button) findViewById(R.id.three);
+        four = (Button) findViewById(R.id.four);
+        five = (Button) findViewById(R.id.five);
+        six = (Button) findViewById(R.id.six);
+        seven = (Button) findViewById(R.id.seven);
+        eight = (Button) findViewById(R.id.eight);
+        nine = (Button) findViewById(R.id.nine);
+        zero = (Button) findViewById(R.id.zero);
+        add = (Button) findViewById(R.id.plus);
+        sub = (Button) findViewById(R.id.minus);
+        mul = (Button) findViewById(R.id.multiply);
+        div = (Button) findViewById(R.id.div);
+        clr = (Button) findViewById(R.id.clr);
+        equal = (Button) findViewById(R.id.equal);
+        */
+        res = (EditText) findViewById(R.id.res);
+
     }
+
+    public void display(View v)
+    {
+        String prev = res.getText().toString();
+        switch(v.getId())
+        {
+            case R.id.one:
+                if (prev.trim().length() == 0)
+                    res.setText("1");
+                else
+                    res.setText(prev+"1");
+                break;
+            case R.id.two:
+                if (prev.trim().length() == 0)
+                    res.setText("2");
+                else
+                    res.setText(prev+"2");
+
+                break;
+            case R.id.three:
+                if (prev.trim().length() == 0)
+                    res.setText("3");
+                else
+                    res.setText(prev+"3");
+
+                break;
+            case R.id.four:
+                if (prev.trim().length() == 0)
+                    res.setText("4");
+                else
+                    res.setText(prev+"4");
+
+                break;
+            case R.id.five:
+                if (prev.trim().length() == 0)
+                    res.setText("5");
+                else
+                    res.setText(prev+"5");
+
+                break;
+            case R.id.six:
+                if (prev.trim().length() == 0)
+                    res.setText("6");
+                else
+                    res.setText(prev+"6");
+
+                break;
+            case R.id.seven:
+                if (prev.trim().length() == 0)
+                    res.setText("7");
+                else
+                    res.setText(prev+"7");
+
+                break;
+            case R.id.eight:
+                if (prev.trim().length() == 0)
+                    res.setText("8");
+                else
+                    res.setText(prev+"8");
+
+                break;
+            case R.id.nine:
+                if (prev.trim().length() == 0)
+                    res.setText("9");
+                else
+                    res.setText(prev+"9");
+
+                break;
+            case R.id.zero:
+                if (prev.trim().length() == 0)
+                    res.setText("0");
+                else
+                    res.setText(prev+"0");
+
+                break;
+            case R.id.clr:
+                res.setText("");
+
+                break;
+            case R.id.plus:
+                if (prev.trim().length() == 0)
+                    res.setError("Operation not allowed");
+                else
+                    res.setText(prev+"+");
+
+                break;
+            case R.id.minus:
+                if (prev.trim().length() == 0)
+                    res.setText("-");
+                else
+                    res.setText(prev+"-");
+
+                break;
+            case R.id.multiply:
+                if (prev.trim().length() == 0)
+                    res.setError("Operation not allowed");
+                else
+                    res.setText(prev+"*");
+
+                break;
+            case R.id.div:
+                if (prev.trim().length() == 0)
+                    res.setError("Operation not allowed");
+                else
+                    res.setText(prev+"/");
+
+                break;
+
+            default:
+                break;
+        }
+
+
+    }
+
 
 
     @Override
